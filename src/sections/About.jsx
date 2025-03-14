282f070e42a9d007482447d7a684ab2b9a9a528d
Shamilvm <162104443+shamil-global@users.noreply.github.com>
2025-03-14 12:21:45 +0530

    about section

---
+++ src/sections/About.jsx
@@ -0,0 +1,136 @@
+import React, { useState } from "react";
+import Globe from "react-globe.gl";
+import Button from "../components/Button";
+
+const About = () => {
+  const [hasCopied, setHasCopied] = useState(false);
+  const handleCopy = () => {
+    navigator.clipboard.writeText("shanushamil086@gmail.com");
+    setHasCopied(true);
+    setTimeout(() => {
+      setHasCopied(false);
+    }, 2000);
+  };
+  return (
+    <section className="c-space my-20">
+      <div className="grid xl:grid-cols-3 xl:grid-rows-3 md:grid-cols-2 grid-cols-1 gap-5 h-full">
+        <div className="col-span-1 xl:row-span-3">
+          <div className="w-full h-full border border-black-300 bg-black-200 rounded-lg sm:p-7 p-4 flex flex-col gap-5">
+            <img
+              src="/assets/grid1.png"
+              alt=""
+              className="w-full sm:h-[276px] h-fit object-contain"
+            />
+            <div>
+              <p className="text-xl font-semibold mb-2 text-white">
+                I'm Shamil
+              </p>
+              <p className="text-[#afb0b6] text-base">
+                With 2 years of experience, I have honed my skills in frontend
+                and backend development.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="col-span-1 xl:row-span-3">
+          <div className="w-full h-full border border-black-300 bg-black-200 rounded-lg sm:p-7 p-4 flex flex-col gap-5">
+            <img
+              src="/assets/grid2.png"
+              alt=""
+              className="w-full sm:h-[276px] h-fit object-contain"
+            />
+            <div className="text-xl font-semibold mb-2 text-white">
+              Tech Stack
+            </div>
+            <div className="text-[#afb0b6] text-base">
+              I specialize in javascript/typescript with a focus on react and
+              next.js ecosystems.
+            </div>
+          </div>
+        </div>
+        <div className="col-span-1 xl:row-span-4">
+          <div className="w-full h-full border border-black-300 bg-black-200 rounded-lg sm:p-7 p-4 flex flex-col gap-5">
+            <div className="rounded-3xl w-full sm:h-[326px] h-fit flex justify-center items-center">
+              <Globe
+                height={326}
+                width={326}
+                backgroundColor="rgba(0,0,0,0)"
+                backgroundImageOpacity={0.5}
+                showAtmosphere
+                showGraticules
+                globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
+                bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
+                // labelsData={[
+                //   {
+                //     lat: 40,
+                //     lng: -100,
+                //     text: "I'm here!",
+                //     color: "white",
+                //     size: 20,
+                //   },
+                // ]}
+              />
+            </div>
+            <div>
+              <p className="text-xl font-semibold mb-2 text-white">
+                I work remotely across most timezones.
+              </p>
+              <p className="text-[#afb0b6] text-base">
+                I'm based in Croatia, with remote work available.
+              </p>
+              <Button
+                name={"Contact Me"}
+                isBeam
+                containerClass={"w-full mt-10"}
+              />
+            </div>
+          </div>
+        </div>
+        <div className="xl:col-span-2 xl:row-span-3">
+          <div className="w-full h-full border border-black-300 bg-black-200 rounded-lg sm:p-7 p-4 flex flex-col gap-5">
+            <img
+              src="/assets/grid3.png"
+              alt=""
+              className="w-full sm:h-[266px] h-fit object-contain"
+            />
+            <div>
+              <p className="text-xl font-semibold mb-2 text-white">
+                My passion for coding
+              </p>
+              <p className="text-[#afb0b6] text-base">
+                I love solving problems and building things through code. Coding
+                isn't just my profession - it is my passion.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="xl:col-span-1 xl:row-span-2">
+          <div className="w-full h-full border border-black-300 bg-black-200 rounded-lg sm:p-7 p-4 flex flex-col gap-5">
+            <img
+              src="/assets/grid4.png"
+              alt=""
+              className="w-full md:h-[126px] sm:h-[276px] h-fit object-cover sm:object-top"
+            />
+            <div className="space-y-2">
+              <p className="text-[#afb0b6] text-base text-center">Contact Me</p>
+              <div
+                className="cursor-pointer flex justify-center items-center gap-2"
+                onClick={handleCopy}
+              >
+                <img
+                  src={hasCopied ? "assets/tick.svg" : "assets/copy.svg"}
+                  alt=""
+                />
+                <p className="lg:text-2xl md:text-xl font-medium text-white">
+                  shanushamil086@gmail.com
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
